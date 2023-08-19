36a094231dc266d45d97bba2e754e79caa345af7
Abdul Jaweed <jdgaming7320@gmail.com>
2023-08-19 21:30:47 +0530

    adding mlflow for experiment tracking but facing some permission issues soon it well be fixed.

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,21 @@
 from pipelines.training_pipeline import training_pipeline
+from zenml.client import Client
 
 if __name__ == '__main__':
     # Run the pipeline
-    training_pipeline(data_path="data\dataset.csv")
+    
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
+    
+    # training_pipeline(data_path="data\dataset.csv")
+    
+   # training_pipeline(data_path=r"file:C:\Users\Admin\AppData\Roaming\zenml\local_stores\e113a655-b713-4bb6-a0c1-507907ec1507\mlruns")
+
+
+
+import os
+
+user_home = os.path.expanduser("~")
+data_path = os.path.join(user_home, "AppData", "Roaming", "zenml", "local_stores", "e113a655-b713-4bb6-a0c1-507907ec1507", "mlruns")
+
+training_pipeline(data_path=f"file:{data_path}")
+
--- steps/model_train.py
+++ steps/model_train.py
@@ -6,7 +6,13 @@ from src.model_dev import LogisticRegressionModel
 from sklearn.base import ClassifierMixin
 from .config import ModelNameConfig
 
-@step
+import mlflow
+from zenml.client import Client
+
+experiment_tracker = Client().active_stack.experiment_tracker
+
+
+@step(experiment_tracker=experiment_tracker.name)
 def train_model(
     X_train: pd.DataFrame,
     X_test: pd.DataFrame,
@@ -27,6 +33,7 @@ def train_model(
     try:
         model = None
         if config.model_name == "LogisticRegression":
+            mlflow.sklearn.autolog()
             model = LogisticRegressionModel()
             trained_model = model.train(X_train, y_train)
             return trained_model
